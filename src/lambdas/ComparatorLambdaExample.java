12dc1b9e60771600096438b5af085ec139dac562
Sharad Shivmath <sharad.shivmath@gmail.com>
2020-09-13 16:06:18 +0530

    intro to lambdas

---
+++ src/lambdas/ComparatorLambdaExample.java
@@ -0,0 +1,22 @@
+package lambdas;
+
+import java.util.Comparator;
+
+public class ComparatorLambdaExample {
+    public static void main(String[] args) {
+        // old way:
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+
+        // using lambda:
+        Comparator<Integer> comparatorLambda = (Integer a, Integer b) -> a.compareTo(b);
+        Comparator<Integer> comparatorLambda2 = (a, b) -> a.compareTo(b);
+        System.out.println("old: " + comparator.compare(3,2));
+        System.out.println("lambda: " + comparatorLambda.compare(3,2));
+        System.out.println("lambda: " + comparatorLambda2.compare(3,2));
+    }
+}
---
+++ src/lambdas/RunnableExample.java
@@ -0,0 +1,26 @@
+package lambdas;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("inside runnable");
+            }
+        };
+        new Thread(runnable).start();
+
+        // using lambda:
+        Runnable runnableLambda = () -> {
+            System.out.println("inside runnable lambda");
+        };
+        new Thread(runnableLambda).run();
+
+        // for single statements, no need of curly braces:
+        Runnable runnableLambda2 = () -> System.out.println("inside runnable lambda 2");
+        new Thread(runnableLambda2).run();
+
+        // all in single line:
+        new Thread(() -> System.out.println("inside runnable lambda 3")).run();
+    }
+}
